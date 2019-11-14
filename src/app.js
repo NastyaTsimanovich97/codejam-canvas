fa9b564f0c6e5034fc35f8dd516e60ce7114c52b
NastyaTsimanovich97 <nastya.cimanovich.97@gmail.com>
2019-11-14 12:57:59 +0300

    fix: add function for active canvas

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
 import './scss/base.scss';
-import './canvas.js'
-
+import './canvas.js';
 
 let canvas4=document.getElementById('canvas4');
 let canvas32=document.getElementById('canvas32');
 let canvas256=document.getElementById('canvas256');
 
-
-document.querySelector('.button__canvas32').addEventListener('click',function(){
-    canvas32.style.display='block';
-    canvas4.style.display='none';
-    canvas256.style.display='none';
-});
-document.querySelector('.button__canvas256').addEventListener('click',function(){
-    canvas256.style.display='block';
-    canvas4.style.display='none';
-    canvas32.style.display='none';
-});
-document.querySelector('.button__canvas4').addEventListener('click',function(){
-    canvas4.style.display='block';
-    canvas32.style.display='none';
-    canvas256.style.display='none';
-});
+document.addEventListener('click',function(e){
+    let button=e.target;
+    if(button.classList.contains('button__canvas32')){
+        activeCanvas(canvas32,canvas4,canvas256);
+    }
+    if(button.classList.contains('button__canvas256')){
+        activeCanvas(canvas256,canvas4,canvas32);
+    }
+    if(button.classList.contains('button__canvas4')){
+        activeCanvas(canvas4,canvas32,canvas256);
+    }
+})
+function activeCanvas(active,prev,next){
+    active.style.display='block';
+    prev.style.display='none';
+    next.style.display='none';
+}
--- src/canvas.js
+++ src/canvas.js
@@ -1,9 +1,10 @@
-import * as colorArray from './assets/data/32x32.json'
+import * as colorArray from './assets/data/32x32.json';
+
 let canvas4=document.getElementById('canvas4').getContext('2d');
 
 canvas4.fillStyle='#00BCD4';
 canvas4.fillRect(0,0,128,128);
-canvas4.fillStyle='#FFEB3B'
+canvas4.fillStyle='#FFEB3B';
 canvas4.fillRect(128,0,256,128);
 canvas4.fillStyle='#00BCD4';
 canvas4.fillRect(384,0,128,128);
@@ -15,7 +16,7 @@ canvas4.fillStyle='#FFEB3B';
 canvas4.fillRect(384,128,128,256);
 canvas4.fillStyle='#00BCD4';
 canvas4.fillRect(0,384,128,128);
-canvas4.fillStyle='#FFEB3B'
+canvas4.fillStyle='#FFEB3B';
 canvas4.fillRect(128,384,256,128);
 canvas4.fillStyle='#00BCD4';
 canvas4.fillRect(384,384,128,128);
