369addccf514a20dde32131c246931d7658dd6dd
NastyaTsimanovich97 <nastya.cimanovich.97@gmail.com>
2019-11-14 13:30:03 +0300

    fix: add function createCanvas with difference size

--- src/app.js
+++ src/app.js
@@ -1,24 +1,2 @@
 import './scss/base.scss';
 import './canvas.js';
-
-let canvas4=document.getElementById('canvas4');
-let canvas32=document.getElementById('canvas32');
-let canvas256=document.getElementById('canvas256');
-
-document.addEventListener('click',function(e){
-    let button=e.target;
-    if(button.classList.contains('button__canvas32')){
-        activeCanvas(canvas32,canvas4,canvas256);
-    }
-    if(button.classList.contains('button__canvas256')){
-        activeCanvas(canvas256,canvas4,canvas32);
-    }
-    if(button.classList.contains('button__canvas4')){
-        activeCanvas(canvas4,canvas32,canvas256);
-    }
-})
-function activeCanvas(active,prev,next){
-    active.style.display='block';
-    prev.style.display='none';
-    next.style.display='none';
-}
--- src/canvas.js
+++ src/canvas.js
@@ -1,38 +1,52 @@
 import * as colorArray from './assets/data/32x32.json';
 
-let canvas4=document.getElementById('canvas4').getContext('2d');
+let canvas=document.getElementById('canvas').getContext('2d');
 
-canvas4.fillStyle='#00BCD4';
-canvas4.fillRect(0,0,128,128);
-canvas4.fillStyle='#FFEB3B';
-canvas4.fillRect(128,0,256,128);
-canvas4.fillStyle='#00BCD4';
-canvas4.fillRect(384,0,128,128);
-canvas4.fillStyle='#FFEB3B';
-canvas4.fillRect(0,128,128,256);
-canvas4.fillStyle='#FFC107';
-canvas4.fillRect(128,128,256,256);
-canvas4.fillStyle='#FFEB3B';
-canvas4.fillRect(384,128,128,256);
-canvas4.fillStyle='#00BCD4';
-canvas4.fillRect(0,384,128,128);
-canvas4.fillStyle='#FFEB3B';
-canvas4.fillRect(128,384,256,128);
-canvas4.fillStyle='#00BCD4';
-canvas4.fillRect(384,384,128,128);
+function createCanvas4(canvas){
+  canvas.fillStyle='#00BCD4';
+  canvas.fillRect(0,0,128,128);
+  canvas.fillStyle='#FFEB3B';
+  canvas.fillRect(128,0,256,128);
+  canvas.fillStyle='#00BCD4';
+  canvas.fillRect(384,0,128,128);
+  canvas.fillStyle='#FFEB3B';
+  canvas.fillRect(0,128,128,256);
+  canvas.fillStyle='#FFC107';
+  canvas.fillRect(128,128,256,256);
+  canvas.fillStyle='#FFEB3B';
+  canvas.fillRect(384,128,128,256);
+  canvas.fillStyle='#00BCD4';
+  canvas.fillRect(0,384,128,128);
+  canvas.fillStyle='#FFEB3B';
+  canvas.fillRect(128,384,256,128);
+  canvas.fillStyle='#00BCD4';
+  canvas.fillRect(384,384,128,128);
+}
+createCanvas4(canvas);
 
-let canvas32=document.getElementById('canvas32').getContext('2d');
-
-
-for (let i=0;i<colorArray.length;i++){
+function createCanvas32(canvas){
+  for (let i=0;i<colorArray.length;i++){
     for (let j=0;j<colorArray[i].length;j++){
-        canvas32.fillStyle='rgb('+colorArray[i][j].join()+')';
-        canvas32.fillRect(j*16,i*16,16,16);
+        canvas.fillStyle='rgb('+colorArray[i][j].join()+')';
+        canvas.fillRect(j*16,i*16,16,16);
     }
+  }
 }
 
-let canvas256 = document.getElementById('canvas256').getContext('2d');
-let img = new Image();
+function createCanvas256(canvas){
+  let img = new Image();
+  canvas.drawImage(document.getElementById('canvas_img'),0,0,512,512);
+}
 
-canvas256.drawImage(document.getElementById('canvas_img'),0,0,512,512);
-
+document.addEventListener('click',function(e){
+  let button=e.target;
+  if(button.classList.contains('button__canvas4')){
+    createCanvas4(canvas);
+}
+  if(button.classList.contains('button__canvas32')){
+    createCanvas32(canvas);
+  }
+  if(button.classList.contains('button__canvas256')){
+    createCanvas256(canvas);
+  }
+})
